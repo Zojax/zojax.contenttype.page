a3801f318326077bc2d2d58440e49bfe3fec2111
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2009-12-24 15:16:01 +0000

    upload the fork of z3ext

---
+++ src/zojax/contenttype/page/page.py
@@ -0,0 +1,61 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" page implementation
+
+$Id$
+"""
+from zope import interface, component
+from zope.size import byteDisplay
+from zope.size.interfaces import ISized
+from zojax.content.type.item import PersistentItem
+from zojax.content.type.searchable import ContentSearchableText
+from zojax.richtext.field import RichTextProperty
+
+from interfaces import IPage
+
+
+class Page(PersistentItem):
+    interface.implements(IPage)
+
+    text = RichTextProperty(IPage['text'])
+
+
+class Sized(object):
+    component.adapts(IPage)
+    interface.implements(ISized)
+
+    def __init__(self, context):
+        self.context = context
+
+        self.size = len(context.title) + \
+                    len(context.description) + \
+                    len(context.text)
+
+    def sizeForSorting(self):
+        return "byte", self.size
+
+    def sizeForDisplay(self):
+        return byteDisplay(self.size)
+
+
+class PageSearchableText(ContentSearchableText):
+    component.adapts(IPage)
+
+    def getSearchableText(self):
+        text = super(PageSearchableText, self).getSearchableText()
+
+        try:
+            return text + u' ' + self.content.text.text
+        except AttributeError:
+            return text

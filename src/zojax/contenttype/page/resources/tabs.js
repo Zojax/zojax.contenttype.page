3779d58216a4f447864b82c22ada63e2e556cbae
Dmitry Ivanchenko <dmitry.ivanchenko@zojax.com>
2013-12-10 17:42:43 +0200

    #557 last css fixes

--- src/zojax/contenttype/page/resources/tabs.js
+++ src/zojax/contenttype/page/resources/tabs.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-    $('.nav-tabs > li a').click(function(){
-        $('.nav-tabs > li').removeClass('active');
+    $('.page-nav-tabs > li a').click(function(){
+        $('.page-nav-tabs > li').removeClass('active');
         $(this).parent().addClass('active');
         $('.tab-content > div').hide('slow');
         $($(this).attr('href')).show('slow');

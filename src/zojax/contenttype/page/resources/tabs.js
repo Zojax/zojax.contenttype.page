f16feabaa630f0b16df6ae83149f8693341cefab
Dmitry Ivanchenko <dmitry.ivanchenko@zojax.com>
2014-01-02 14:13:08 +0200

    #557 new tabs animation

--- src/zojax/contenttype/page/resources/tabs.js
+++ src/zojax/contenttype/page/resources/tabs.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
-
     $('.page-nav-tabs > li a').click(function(){
+        if ($(this).parent().hasClass('active'))
+            return false;
         $('.page-nav-tabs > li').removeClass('active');
         $(this).parent().addClass('active');
-        $('.tab-content > div').hide('slow');
-        $($(this).attr('href')).show('slow');
+        var self = this;
+        $('.tab-content > div:visible').hide("slide", { direction: "left" },  200, function(){
+            $($(self).attr('href')).show("slide", { direction: "left" }, 200);
+        });
         return false;
     });
 });
